a90e4014c214275320a57f3059ce5801db971e71
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2024-02-28 14:00:02 -0500

    changes in format img and tag for each heading

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'selector',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#f5f5f5",
+				secondary: "#0a0a0a",
+				secondary2: "#0c91a6",
+				primaryDark: "#0a0a0a",
+				secondaryDark: "#f5f5f5",
+				secondary2Dark: "#22d3ee",
+			},
+		},
 	},
 	plugins: [],
 }
