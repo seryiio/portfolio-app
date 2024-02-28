aaf5d91a4218af3e0a3c8b81f2d7a80b6aa8712b
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2024-02-28 17:10:41 -0500

    add animation npm

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import animations from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,12 +9,12 @@ export default {
 			colors: {
 				primary: "#f5f5f5",
 				secondary: "#0a0a0a",
-				secondary2: "#0c91a6",
+				secondary2: "#22d3ee",
 				primaryDark: "#0a0a0a",
 				secondaryDark: "#f5f5f5",
 				secondary2Dark: "#22d3ee",
 			},
 		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
